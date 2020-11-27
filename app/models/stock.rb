653a0e1dfb0a144fb4826d033670c12996048010
Ridge <ridgeamaro@hotmail.com>
2020-11-27 13:44:33 +0200

    Implement stock trading restrictions and force upcase on stock search (duplicates were allowed with different variations) [Figured this out on my own]

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,7 +9,7 @@ class Stock < ApplicationRecord
                                   secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
